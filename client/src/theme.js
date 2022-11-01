cdb47d406cda0eab346a3a381cc7a14268f15bce
Pramod <pramodvitpune@gmail.com>
2022-11-01 12:53:07 +0530

    Added theme base

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,8 +3,25 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     mode: "light",
+    primary: {
+      light: "#7986cb",
+      main: "#3f51b5",
+      dark: "#303f9f",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff4081",
+      main: "#f50057",
+      dark: "#c51162",
+      contrastText: "#fff",
+    },
+    error: {
+      light: "#e57373",
+      main: "#f44336",
+      dark: "#d32f2f",
+      contrastText: "#fff",
+    },
 
-    // error: {},
     // warning: {},
     // info: {},
     // success: {},
